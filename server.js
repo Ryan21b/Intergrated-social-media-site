6f1b1f225b6eb3965769436ca67ce11a07f981ff
Ryan21b <barronryan65@gmail.com>
2020-08-02 16:04:46 +0200

    deploy commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const router = express.Router();
+const path = require("path");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -17,3 +18,11 @@ app.use(express.json());
 
 app.use(require("./routes/auth"));
 app.use(require("./routes/posts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
